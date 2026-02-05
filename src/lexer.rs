04bb42b02d59aeca7a8b95d83ac114d36729c59d
DaryllLorenzo <vikingwarrior4444@gmail.com>
2026-02-05 06:35:01 -0500

    refactor(lexer): replace old tokenizer with stateful Lexer
    - Implement Lexer struct with position, read_position, and line tracking
    - Add support for multi-digit numbers and whitespace handling
    - Include line number in Token for future error reporting
    - Expose lexer as public library via src/lib.rs
    - Simplify main.rs to minimal banner

--- src/lexer.rs
+++ src/lexer.rs
@@ -1,51 +1,143 @@
-use std::iter::{self, from_fn};
-
-pub enum Token {
+#[derive(Clone, Debug, PartialEq)]
+pub enum TokenType {
     Number(i64),
     Plus,
     Dash,
     Star,
-    EOF
+    EOF,
 }
 
-#[derive(Debug)]
-pub struct SyntaxError {
-    pub message: String,
+#[derive(Clone, Debug, PartialEq)]
+pub struct Token {
+    pub token_type: TokenType,
+    pub literal: String,
+    pub line: usize,
 }
 
-impl SyntaxError {
-    fn new(message: String) -> Self {
-        SyntaxError {
-            message
+pub struct Lexer {
+    input: Vec<u8>,
+    position: usize,
+    read_position: usize,
+    ch: u8,
+    line: usize,
+}
+
+impl Lexer {
+    pub fn new(input: String) -> Self {
+        let mut lexer = Lexer {
+            input: input.into_bytes(),
+            position: 0,
+            read_position: 0,
+            ch: 0,
+            line: 1,
+        };
+        lexer.read_char();
+        lexer
+    }
+
+    fn read_char(&mut self) {
+        let prev_ch = self.ch;
+
+        if self.read_position >= self.input.len() {
+            self.ch = 0;
+        } else {
+            self.ch = self.input[self.read_position];
+        }
+
+        self.position = self.read_position;
+        self.read_position += 1;
+
+        if prev_ch == b'\n' {
+            self.line += 1;
         }
     }
-}
 
+    fn read_number(&mut self) -> String {
+        let start = self.position;
+        while self.ch.is_ascii_digit() {
+            self.read_char();
+        }
+        let end = self.position;
+        std::str::from_utf8(&self.input[start..end])
+            .unwrap_or("")
+            .to_string()
+    }
 
-pub fn tokenizer(input: String) -> Result<Vec<Token>, SyntaxError> {
-    let mut tokens: Vec<Token> = Vec::new();
-    let mut iter = input.chars().peekable();
+    pub fn next_token(&mut self) -> Token {
+        // Skip whitespace
+        while matches!(self.ch, b' ' | b'\t' | b'\n' | b'\r') {
+            self.read_char();
+        }
 
-    while let Some(ch) = iter.next() {
-        match ch {
-            ch if ch.is_whitespace() => continue,
-            '+' => tokens.push(Token::Plus),
-            '-' => tokens.push(Token::Dash),
-            '*' => tokens.push(Token::Star),
-            '1'..='9' => {
-                let n: i64 = iter::once(ch)
-                    .chain(from_fn(|| iter.by_ref().next_if(|s| s.is_ascii_digit())))
-                    .collect::<String>()
-                    .parse()
-                    .unwrap();
+        let current_line = self.line;
 
-                tokens.push(Token::Number(n));
+        match self.ch {
+            b'+' => {
+                let tok = Token {
+                    token_type: TokenType::Plus,
+                    literal: "+".to_string(),
+                    line: current_line,
+                };
+                self.read_char();
+                tok
             }
-            _ => return Err(SyntaxError::new(format!("unrecognized character {}", ch))),
+            b'-' => {
+                let tok = Token {
+                    token_type: TokenType::Dash,
+                    literal: "-".to_string(),
+                    line: current_line,
+                };
+                self.read_char();
+                tok
+            }
+            b'*' => {
+                let tok = Token {
+                    token_type: TokenType::Star,
+                    literal: "*".to_string(),
+                    line: current_line,
+                };
+                self.read_char();
+                tok
+            }
+            b'0'..=b'9' => {
+                let literal = self.read_number();
+                let value = literal.parse().unwrap_or(0);
+                Token {
+                    token_type: TokenType::Number(value),
+                    literal,
+                    line: current_line,
+                }
+            }
+            0 => Token {
+                token_type: TokenType::EOF,
+                literal: "".to_string(),
+                line: current_line,
+            },
+            _ => {
+                let literal = format!("{}", self.ch as char);
+                self.read_char();
+                Token {
+                    token_type: TokenType::EOF,
+                    literal,
+                    line: current_line,
+                }
+            }
+        }
+    }
+}
+
+pub fn tokenize(input: String) -> Result<Vec<Token>, ()> {
+    let mut lexer = Lexer::new(input);
+    let mut tokens = Vec::new();
+
+    loop {
+        let token = lexer.next_token();
+        tokens.push(token.clone());
+
+        if matches!(token.token_type, TokenType::EOF) {
+            break;
         }
     }
 
-    tokens.push(Token::EOF);
     Ok(tokens)
 }
-
--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,3 @@
-mod lexer;
-use lexer::{tokenizer, Token};
-
 fn main() {
-    let input = String::from("1 + 2 * 3");
-    
-    match tokenizer(input) {
-        Ok(tokens) => {
-            for token in tokens {
-                match token {
-                    Token::Number(n) => println!("Number: {}", n),
-                    Token::Plus => println!("Operator: +"),
-                    Token::Dash => println!("Operator: -"),
-                    Token::Star => println!("Operator: *"),
-                    Token::EOF => println!("EOF"),
-                }
-            }
-        }
-        Err(e) => println!("Error: {}", e.message),
-    }
+    println!("pseudocu Interpreter");
 }
