96fd8b50b5457960a3898902a3a020286952f41b
DaryllLorenzo <vikingwarrior4444@gmail.com>
2026-02-17 06:51:58 -0500

    feat(lexer): add identifiers and comparison operators

--- src/lexer.rs
+++ src/lexer.rs
@@ -1,9 +1,18 @@
 #[derive(Clone, Debug, PartialEq)]
 pub enum TokenType {
     Number(i64),
+    Ident(String),
+    Assign,
     Plus,
     Minus,
     Star,
+    Slash,
+    Eq,
+    NotEq,
+    Gt,
+    Lt,
+    GtEq,
+    LtEq,
     EOF,
 }
 
@@ -63,6 +72,17 @@ impl Lexer {
             .to_string()
     }
 
+    fn read_ident(&mut self) -> String {
+        let start = self.position;
+        while self.ch.is_ascii_alphabetic() || self.ch.is_ascii_digit() || self.ch == b'_' {
+            self.read_char();
+        }
+        let end = self.position;
+        std::str::from_utf8(&self.input[start..end])
+            .unwrap_or("")
+            .to_string()
+    }
+
     pub fn next_token(&mut self) -> Token {
         // Skip whitespace
         while matches!(self.ch, b' ' | b'\t' | b'\n' | b'\r') {
@@ -72,6 +92,75 @@ impl Lexer {
         let current_line = self.line;
 
         match self.ch {
+            b'=' => {
+                self.read_char();
+                if self.ch == b'=' {
+                    self.read_char();
+                    Token {
+                        token_type: TokenType::Eq,
+                        literal: "==".to_string(),
+                        line: current_line,
+                    }
+                } else {
+                    Token {
+                        token_type: TokenType::Assign,
+                        literal: "=".to_string(),
+                        line: current_line,
+                    }
+                }
+            }
+            b'!' => {
+                self.read_char();
+                if self.ch == b'=' {
+                    self.read_char();
+                    Token {
+                        token_type: TokenType::NotEq,
+                        literal: "!=".to_string(),
+                        line: current_line,
+                    }
+                } else {
+                    let literal = "!".to_string();
+                    Token {
+                        token_type: TokenType::EOF,
+                        literal,
+                        line: current_line,
+                    }
+                }
+            }
+            b'>' => {
+                self.read_char();
+                if self.ch == b'=' {
+                    self.read_char();
+                    Token {
+                        token_type: TokenType::GtEq,
+                        literal: ">=".to_string(),
+                        line: current_line,
+                    }
+                } else {
+                    Token {
+                        token_type: TokenType::Gt,
+                        literal: ">".to_string(),
+                        line: current_line,
+                    }
+                }
+            }
+            b'<' => {
+                self.read_char();
+                if self.ch == b'=' {
+                    self.read_char();
+                    Token {
+                        token_type: TokenType::LtEq,
+                        literal: "<=".to_string(),
+                        line: current_line,
+                    }
+                } else {
+                    Token {
+                        token_type: TokenType::Lt,
+                        literal: "<".to_string(),
+                        line: current_line,
+                    }
+                }
+            }
             b'+' => {
                 let tok = Token {
                     token_type: TokenType::Plus,
@@ -99,11 +188,28 @@ impl Lexer {
                 self.read_char();
                 tok
             }
+            b'/' => {
+                let tok = Token {
+                    token_type: TokenType::Slash,
+                    literal: "/".to_string(),
+                    line: current_line,
+                };
+                self.read_char();
+                tok
+            }
             b'0'..=b'9' => {
                 let literal = self.read_number();
                 let value = literal.parse().unwrap_or(0);
                 Token {
                     token_type: TokenType::Number(value),
+                    literal,
+                    line: current_line,
+                }
+            }
+            b'a'..=b'z' | b'A'..=b'Z' | b'_' => {
+                let literal = self.read_ident();
+                Token {
+                    token_type: TokenType::Ident(literal.clone()),
                     literal,
                     line: current_line,
                 }
--- tests/lexer.rs
+++ tests/lexer.rs
@@ -63,3 +63,322 @@ fn test_whitespace_and_newlines() {
         line: 2,
     });
 }
+
+// ==================== Identifier Tests ====================
+
+#[test]
+fn test_identifier() {
+    let input = "x".to_string();
+    let mut lexer = Lexer::new(input);
+
+    assert_eq!(lexer.next_token(), Token {
+        token_type: TokenType::Ident("x".to_string()),
+        literal: "x".to_string(),
+        line: 1,
+    });
+    assert_eq!(lexer.next_token(), Token {
+        token_type: TokenType::EOF,
+        literal: "".to_string(),
+        line: 1,
+    });
+}
+
+#[test]
+fn test_identifier_with_underscore() {
+    let input = "my_var".to_string();
+    let mut lexer = Lexer::new(input);
+
+    assert_eq!(lexer.next_token(), Token {
+        token_type: TokenType::Ident("my_var".to_string()),
+        literal: "my_var".to_string(),
+        line: 1,
+    });
+    assert_eq!(lexer.next_token(), Token {
+        token_type: TokenType::EOF,
+        literal: "".to_string(),
+        line: 1,
+    });
+}
+
+#[test]
+fn test_identifier_with_numbers() {
+    let input = "var123".to_string();
+    let mut lexer = Lexer::new(input);
+
+    assert_eq!(lexer.next_token(), Token {
+        token_type: TokenType::Ident("var123".to_string()),
+        literal: "var123".to_string(),
+        line: 1,
+    });
+    assert_eq!(lexer.next_token(), Token {
+        token_type: TokenType::EOF,
+        literal: "".to_string(),
+        line: 1,
+    });
+}
+
+#[test]
+fn test_identifier_uppercase() {
+    let input = "MyVariable".to_string();
+    let mut lexer = Lexer::new(input);
+
+    assert_eq!(lexer.next_token(), Token {
+        token_type: TokenType::Ident("MyVariable".to_string()),
+        literal: "MyVariable".to_string(),
+        line: 1,
+    });
+    assert_eq!(lexer.next_token(), Token {
+        token_type: TokenType::EOF,
+        literal: "".to_string(),
+        line: 1,
+    });
+}
+
+// ==================== Assignment Operator Tests ====================
+
+#[test]
+fn test_assignment_operator() {
+    let input = "x = 5".to_string();
+    let mut lexer = Lexer::new(input);
+
+    assert_eq!(lexer.next_token(), Token {
+        token_type: TokenType::Ident("x".to_string()),
+        literal: "x".to_string(),
+        line: 1,
+    });
+    assert_eq!(lexer.next_token(), Token {
+        token_type: TokenType::Assign,
+        literal: "=".to_string(),
+        line: 1,
+    });
+    assert_eq!(lexer.next_token(), Token {
+        token_type: TokenType::Number(5),
+        literal: "5".to_string(),
+        line: 1,
+    });
+    assert_eq!(lexer.next_token(), Token {
+        token_type: TokenType::EOF,
+        literal: "".to_string(),
+        line: 1,
+    });
+}
+
+// ==================== Division Operator Tests ====================
+
+#[test]
+fn test_division_operator() {
+    let input = "10 / 2".to_string();
+    let mut lexer = Lexer::new(input);
+
+    assert_eq!(lexer.next_token(), Token {
+        token_type: TokenType::Number(10),
+        literal: "10".to_string(),
+        line: 1,
+    });
+    assert_eq!(lexer.next_token(), Token {
+        token_type: TokenType::Slash,
+        literal: "/".to_string(),
+        line: 1,
+    });
+    assert_eq!(lexer.next_token(), Token {
+        token_type: TokenType::Number(2),
+        literal: "2".to_string(),
+        line: 1,
+    });
+    assert_eq!(lexer.next_token(), Token {
+        token_type: TokenType::EOF,
+        literal: "".to_string(),
+        line: 1,
+    });
+}
+
+// ==================== Comparison Operator Tests ====================
+
+#[test]
+fn test_equal_comparison() {
+    let input = "x == y".to_string();
+    let mut lexer = Lexer::new(input);
+
+    assert_eq!(lexer.next_token(), Token {
+        token_type: TokenType::Ident("x".to_string()),
+        literal: "x".to_string(),
+        line: 1,
+    });
+    assert_eq!(lexer.next_token(), Token {
+        token_type: TokenType::Eq,
+        literal: "==".to_string(),
+        line: 1,
+    });
+    assert_eq!(lexer.next_token(), Token {
+        token_type: TokenType::Ident("y".to_string()),
+        literal: "y".to_string(),
+        line: 1,
+    });
+    assert_eq!(lexer.next_token(), Token {
+        token_type: TokenType::EOF,
+        literal: "".to_string(),
+        line: 1,
+    });
+}
+
+#[test]
+fn test_not_equal_comparison() {
+    let input = "a != b".to_string();
+    let mut lexer = Lexer::new(input);
+
+    assert_eq!(lexer.next_token(), Token {
+        token_type: TokenType::Ident("a".to_string()),
+        literal: "a".to_string(),
+        line: 1,
+    });
+    assert_eq!(lexer.next_token(), Token {
+        token_type: TokenType::NotEq,
+        literal: "!=".to_string(),
+        line: 1,
+    });
+    assert_eq!(lexer.next_token(), Token {
+        token_type: TokenType::Ident("b".to_string()),
+        literal: "b".to_string(),
+        line: 1,
+    });
+    assert_eq!(lexer.next_token(), Token {
+        token_type: TokenType::EOF,
+        literal: "".to_string(),
+        line: 1,
+    });
+}
+
+#[test]
+fn test_greater_than() {
+    let input = "5 > 3".to_string();
+    let mut lexer = Lexer::new(input);
+
+    assert_eq!(lexer.next_token(), Token {
+        token_type: TokenType::Number(5),
+        literal: "5".to_string(),
+        line: 1,
+    });
+    assert_eq!(lexer.next_token(), Token {
+        token_type: TokenType::Gt,
+        literal: ">".to_string(),
+        line: 1,
+    });
+    assert_eq!(lexer.next_token(), Token {
+        token_type: TokenType::Number(3),
+        literal: "3".to_string(),
+        line: 1,
+    });
+    assert_eq!(lexer.next_token(), Token {
+        token_type: TokenType::EOF,
+        literal: "".to_string(),
+        line: 1,
+    });
+}
+
+#[test]
+fn test_less_than() {
+    let input = "2 < 10".to_string();
+    let mut lexer = Lexer::new(input);
+
+    assert_eq!(lexer.next_token(), Token {
+        token_type: TokenType::Number(2),
+        literal: "2".to_string(),
+        line: 1,
+    });
+    assert_eq!(lexer.next_token(), Token {
+        token_type: TokenType::Lt,
+        literal: "<".to_string(),
+        line: 1,
+    });
+    assert_eq!(lexer.next_token(), Token {
+        token_type: TokenType::Number(10),
+        literal: "10".to_string(),
+        line: 1,
+    });
+    assert_eq!(lexer.next_token(), Token {
+        token_type: TokenType::EOF,
+        literal: "".to_string(),
+        line: 1,
+    });
+}
+
+#[test]
+fn test_greater_than_or_equal() {
+    let input = "x >= 5".to_string();
+    let mut lexer = Lexer::new(input);
+
+    assert_eq!(lexer.next_token(), Token {
+        token_type: TokenType::Ident("x".to_string()),
+        literal: "x".to_string(),
+        line: 1,
+    });
+    assert_eq!(lexer.next_token(), Token {
+        token_type: TokenType::GtEq,
+        literal: ">=".to_string(),
+        line: 1,
+    });
+    assert_eq!(lexer.next_token(), Token {
+        token_type: TokenType::Number(5),
+        literal: "5".to_string(),
+        line: 1,
+    });
+    assert_eq!(lexer.next_token(), Token {
+        token_type: TokenType::EOF,
+        literal: "".to_string(),
+        line: 1,
+    });
+}
+
+#[test]
+fn test_less_than_or_equal() {
+    let input = "y <= 100".to_string();
+    let mut lexer = Lexer::new(input);
+
+    assert_eq!(lexer.next_token(), Token {
+        token_type: TokenType::Ident("y".to_string()),
+        literal: "y".to_string(),
+        line: 1,
+    });
+    assert_eq!(lexer.next_token(), Token {
+        token_type: TokenType::LtEq,
+        literal: "<=".to_string(),
+        line: 1,
+    });
+    assert_eq!(lexer.next_token(), Token {
+        token_type: TokenType::Number(100),
+        literal: "100".to_string(),
+        line: 1,
+    });
+    assert_eq!(lexer.next_token(), Token {
+        token_type: TokenType::EOF,
+        literal: "".to_string(),
+        line: 1,
+    });
+}
+
+// ==================== Complete Program Test ====================
+
+#[test]
+fn test_complete_assignment_program() {
+    let input = "d = 5\na = 4\nresult = d + a".to_string();
+    let mut lexer = Lexer::new(input);
+
+    // d = 5
+    assert_eq!(lexer.next_token().token_type, TokenType::Ident("d".to_string()));
+    assert_eq!(lexer.next_token().token_type, TokenType::Assign);
+    assert_eq!(lexer.next_token().token_type, TokenType::Number(5));
+    
+    // a = 4
+    assert_eq!(lexer.next_token().token_type, TokenType::Ident("a".to_string()));
+    assert_eq!(lexer.next_token().token_type, TokenType::Assign);
+    assert_eq!(lexer.next_token().token_type, TokenType::Number(4));
+    
+    // result = d + a
+    assert_eq!(lexer.next_token().token_type, TokenType::Ident("result".to_string()));
+    assert_eq!(lexer.next_token().token_type, TokenType::Assign);
+    assert_eq!(lexer.next_token().token_type, TokenType::Ident("d".to_string()));
+    assert_eq!(lexer.next_token().token_type, TokenType::Plus);
+    assert_eq!(lexer.next_token().token_type, TokenType::Ident("a".to_string()));
+    
+    assert_eq!(lexer.next_token().token_type, TokenType::EOF);
+}
